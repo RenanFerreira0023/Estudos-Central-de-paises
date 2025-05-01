4bcc4644c109481759edabfed9e00bf7b7d2d37e
Renan Dutra <appsskilldeveloper@gmail.com>
2025-05-01 15:41:11 -0300

    consumindo dados de uma api  externa  mas ainda em nivel de fontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,37 @@
-import { Card, Footer, Header ,Grid } from './componets/index'
-
-const countires = [
-  {
-    id: 1,
-    pais: 'Brasil',
-    capital: 'Brasília',
-    regiao: 'América do Sul',
-    populacao: '214000000'
-  },
-  {
-    id: 2,
-    pais: 'Argentina',
-    capital: 'Buenos Aires',
-    regiao: 'América do Sul',
-    populacao: '46000000'
-  },
-  {
-    id: 3,
-    pais: 'Canadá',
-    capital: 'Ottawa',
-    regiao: 'América do Norte',
-    populacao: '39000000'
-  },
-  {
-    id: 4,
-    pais: 'Alemanha',
-    capital: 'Berlim',
-    regiao: 'Europa',
-    populacao: '83000000'
-  },
-  {
-    id: 5,
-    pais: 'Japão',
-    capital: 'Tóquio',
-    regiao: 'Ásia',
-    populacao: '125000000'
-  },
-  {
-    id: 6,
-    pais: 'Austrália',
-    capital: 'Camberra',
-    regiao: 'Oceania',
-    populacao: '26000000'
-  },
-  {
-    id: 7,
-    pais: 'Nigéria',
-    capital: 'Abuja',
-    regiao: 'África',
-    populacao: '223000000'
-  },
-  {
-    id: 8,
-    pais: 'México',
-    capital: 'Cidade do México',
-    regiao: 'América do Norte',
-    populacao: '129000000'
-  },
-  {
-    id: 9,
-    pais: 'França',
-    capital: 'Paris',
-    regiao: 'Europa',
-    populacao: '68000000'
-  },
-  {
-    id: 10,
-    pais: 'Índia',
-    capital: 'Nova Délhi',
-    regiao: 'Ásia',
-    populacao: '1410000000'
-  }
-];
-
-
+'use client';
+import { useEffect , useState} from 'react';
+import { Card, Footer, Header, Grid } from './componets/index';
 
 export default function Home() {
+
+const [countries, setCountries] = useState([]);
+
+
+  useEffect(() => {
+    const fetchcountries = async () => {
+      const response = await fetch("https://restcountries.com/v3.1/all");
+      const data = await response.json()
+      console.log(data)
+      setCountries(data)
+}
+
+    fetchcountries();
+  }, []);
+
   return (
     <>
       <Header />
       <main className="flex-1">
-          <Grid>
-            {countires.map(({ id, pais, capital, regiao, populacao }) => (
-              <Card
-                key={id}
-                pais={pais}
-                capital={capital}
-                regiao={regiao}
-                populacao={populacao} />
-            ))}
-          </Grid>
+        <Grid>
+          {countries.map(({ id, name, capital, region, population }) => (
+            <Card
+              key={id}
+              pais={name.common}
+              capital={capital}
+              regiao={region}
+              populacao={population} />
+          ))}
+        </Grid>
       </main>
       <Footer />
     </>
