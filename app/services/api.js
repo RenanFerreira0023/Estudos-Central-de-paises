911bde6adaaf433bdf008f214c9c81953aab607c
Renan Dutra <appsskilldeveloper@gmail.com>
2025-05-01 21:13:35 -0300

    finalziado minimamente para fazer deploy

--- app/services/api.js
+++ app/services/api.js
@@ -1,17 +1,3 @@
-//ry {
-//   const response = await fetch("https://restcountries.com/v3.1/all?fields=cca3,flags,name,capital,region,population");
-//   const data = await response.json();
-//   console.log(data);
-//   setCountries(data);
-//   setLoading(false); // Após a requisição, setamos loading como false
-// } catch (error) {
-//   setError(`Deu um erro: ${error}`);
-//   console.log(error)
-//   setLoading(false); // Mesmo em caso de erro, setamos loading como false
-// }finally{
-//   setLoading(false)
-// }
-
 const ApiClient = (baseUrl) => ({
   async get(endpoint) {
     try {
